9a8f2049bcfd14b42845c6be7a5c7ceb17961520
cubetastic33 <aravkasi@gmail.com>
2023-09-01 02:09:47 -0500

    Redo whole thing

---
+++ order.js
@@ -0,0 +1,112 @@
+const $ = id => document.getElementById(id);
+const $count = $('count');
+const $form = $('form');
+
+$('sand').addEventListener('click', () => {
+  $('form').dataset.color = 'sand';
+  $('one').classList.remove('active');
+  $('two').classList.add('active');
+});
+
+$('chocolate').addEventListener('click', () => {
+  $('form').dataset.color = 'chocolate';
+  $('one').classList.remove('active');
+  $('two').classList.add('active');
+});
+
+// Screen two
+
+document.querySelectorAll('ul li').forEach(elem => {
+  elem.addEventListener('click', e => {
+    document.querySelectorAll('ul li').forEach(li => li.classList.remove('selected'));
+    e.target.classList.add('selected');
+  });
+});
+
+$('less').addEventListener('click', e => {
+  e.preventDefault();
+  if (parseInt($count.innerText) > 1)
+    $count.innerText = (parseInt($count.innerText) - 1).toString();
+});
+
+$('more').addEventListener('click', e => {
+  e.preventDefault();
+  $count.innerText = (parseInt($count.innerText) + 1).toString();
+});
+
+$('back').addEventListener('click', e => {
+  e.preventDefault();
+  $('two').classList.remove('active');
+  $('one').classList.add('active');
+});
+
+$('proceed').addEventListener('click', e => {
+  e.preventDefault();
+  $('two').classList.remove('active');
+  $('three').classList.add('active');
+});
+
+// Screen three
+
+$('back2').addEventListener('click', e => {
+  e.preventDefault();
+  $('three').classList.remove('active');
+  $('two').classList.add('active');
+});
+
+$('proceed2').addEventListener('click', e => {
+  e.preventDefault();
+  $('three').classList.remove('active');
+  $('four').classList.add('active');
+});
+
+// Screen four
+
+$('back3').addEventListener('click', e => {
+  e.preventDefault();
+  $('three').classList.add('active');
+  $('four').classList.remove('active');
+});
+
+$('noShipping').addEventListener('click', e => {
+  e.preventDefault();
+  $('shipping').classList.add('hide');
+  $('four').classList.remove('active');
+  $('five').classList.add('active');
+});
+
+$('yesShipping').addEventListener('click', e => {
+  e.preventDefault();
+  if ($('address1').value.trim().length === 0) {
+    alert('Please enter a shipping address!');
+    return;
+  }
+  $('shipping').classList.remove('hide');
+  $('four').classList.remove('active');
+  $('five').classList.add('active');
+});
+
+// Screen five
+
+$('back4').addEventListener('click', e => {
+  e.preventDefault();
+  $('four').classList.add('active');
+  $('five').classList.remove('active');
+});
+
+$('order').addEventListener('click', e => {
+  e.preventDefault();
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzGPGr0c96QdIKm5n_L0Tc-myQoMJXVxXksw8Mq7Z8/dev';
+  let requestBody = new FormData($form);
+  requestBody.set('color', $form.dataset.color);
+  requestBody.set('size', document.querySelector('#size .selected').innerText);
+  requestBody.set('number', $('count').innerText);
+  console.log(requestBody)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+      console.log('Success!', response);
+    })
+    .catch(error => {
+      console.log('Error!', error.message);
+    });
+});
