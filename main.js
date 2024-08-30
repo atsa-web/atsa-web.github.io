ce640f12c1fdc458d56d3e6ca933a28d1e239283
aravk <aravkasi@gmail.com>
2024-08-30 17:09:57 -0500

    Try to fix bugs

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const $ = id => document.getElementById(id);
 
 const LILY = .5;
-const SURROUNDING_TEXT = 1.5;
-const INTERESTED = 3.5;
+const SURROUNDING_TEXT = 1;
+const INTERESTED = 2;
 
 function getIntroOpacity() {
   return Math.max(0, 1 - window.scrollY / window.innerHeight * 2);
@@ -10,7 +10,7 @@ function getIntroOpacity() {
 
 function getOpacity(start) {
   start *= window.innerHeight;
-  const maxesAt = start + window.innerHeight / 2;
+  const maxesAt = window.innerHeight;
   if (window.scrollY > start)
     return Math.max(0, Math.min(maxesAt, window.scrollY - start) / maxesAt);
   else
